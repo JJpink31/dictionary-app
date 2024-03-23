d2f1a5246a18e9378adea9c6225216f2103af1a0
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2024-03-23 18:29:07 -0500

    trying to render the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data.meanings[0]);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Search for the defintion of ${keyword}`);
 
-    let apiKey = "f8833caao3caf01e1ffbc8t348acfb03";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=f8833caao3caf01e1ffbc8t348acfb03`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -27,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
