b1c6193c328f81e051fdf9c5ae661009521eefe5
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2024-03-23 13:01:04 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Search for the defintion of ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
-    <div className="greeting">
-      <h1>hello</h1>
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
     </div>
   );
 }
