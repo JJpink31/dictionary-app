e31e2c99a2389b0ae6fe116cec55e53dff262f5b
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2024-03-23 14:12:32 -0500

    imported a dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Search for the defintion of ${keyword}`);
+
+    let apiKey = "f8833caao3caf01e1ffbc8t348acfb03";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
