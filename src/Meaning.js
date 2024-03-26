35861577a01f26555d83d47cfcd0a7074d01f243
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2024-03-26 10:17:07 -0500

    updated meaning.js and synonyms.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,20 +5,19 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
+      {props.meaning.definitions.slice(0, 2).map(function (definition, index) {
         return (
           <div key={index}>
             <div className="definition">
               <strong>Definition</strong>
               {definition.definition}
             </div>
-            <Synonyms synonyms={definition.synonyms} />
-            <p>
+            <div className="example">
               <em>
-                <strong>Example</strong>
-                {definition.example}
+                <strong>Example:</strong> {definition.example}
               </em>
-            </p>
+            </div>
+            <Synonyms synonyms={props.meaning.synonyms} />
             <br />
           </div>
         );
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul>
-        {props.synonyms.map(function (synonyms, index) {
+        {props.synonyms.slice(0, 5).map(function (synonyms, index) {
           return <li key={index}>{synonyms}</li>;
         })}
       </ul>
