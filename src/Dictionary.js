be85743c0a27b0efe110c0cab26e7be695b1e9c4
JJpink31 <140741161+JJpink31@users.noreply.github.com>
2024-03-29 22:51:49 -0500

    imported Photo API from Pexels

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [results, setResults] = useState(null);
   let [keyword, setKeyword] = useState("");
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "BYZWo9KWiFRP53944yTPSFcAglD68OplocWp9DgIB7bcRQ5mLFTVxjv4";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -38,6 +49,7 @@ export default function Dictionary() {
       </div>
       <section>
         <Results results={results} />
+        <Photos photos={photos} />
       </section>
     </div>
   );
